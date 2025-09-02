fdfeb34ad6797e1c579bd9056ca9979552e539aa
hihave <asebaa200@gmail.com>
2025-09-02 14:01:57 +0100

    Qst4

--- chapitre4/Qst4 .c
+++ chapitre4/Qst4 .c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
 main(){
-    int taxe;
-    float MT;
+    float MT,taxe;
+    
+    printf("Entrer un montant en dollars:\n");
+    scanf("%f",&MT);
+
+    taxe=MT*0.05;
+    MT=MT+taxe;
+
+    printf("Le montant avec les texes est : %.2f\n",MT);
+    return 0;
 
 }
